994fcbdec2c347d92d650629d1a838beb19d347d
maestraTin <09christine.garcia@gmail.com>
2021-12-14 23:11:31 +0800

    add

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <Fragment>
+      <Container className="mr-auto">
+        <h1> This is Footer</h1>
+      </Container>
+    </Fragment>
+  )
